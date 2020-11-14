20c5ca56013e83553c9eff227ddd6c6ccdcbce13
Drake Mossman <drake.mossman@covenanteyes.com>
2020-11-14 13:20:23 -0500

    Build and type definition improvements
    Add type for solution
    Make output directory /solution
    Add postbuild script to copy .js output and trim out anything superfluous to solution

--- elevator.ts
+++ elevator.ts
@@ -2,33 +2,33 @@
 interface elevator {
     /** Queue the elevator to go to specified floor number. If you specify true as second argument, the elevator will go
      * to that floor directly, and then go to any other queued floors. */
-    goToFloor: (floorNum: number, addToBeginning?: boolean) => void
+    goToFloor: (floorNum: number, addToBeginning?: boolean) => void;
     /** Clear the destination queue and stop the elevator if it is moving. Note that you normally don't need to stop
      * elevators - it is intended for advanced solutions with in-transit rescheduling logic. Also, note that the
      * elevator will probably not stop at a floor, so passengers will not get out. */
-    stop: () => void,
+    stop: () => void;
     /** Gets the floor number that the elevator currently is on. */
-    currentFloor: () => number,
+    currentFloor: () => number;
     /** Gets or sets the going up indicator, which will affect passenger behaviour when stopping at floors. */
-    goingUpIndicator: (() => boolean) | ((newState: boolean) => void),
+    goingUpIndicator: (() => boolean) | ((newState: boolean) => void);
     /** Gets or sets the going down indicator, which will affect passenger behaviour when stopping at floors. */
-    goingDownIndicator: (() => boolean) | ((newState: boolean) => void),
+    goingDownIndicator: (() => boolean) | ((newState: boolean) => void);
     /** Gets the maximum number of passengers that can occupy the elevator at the same time. */
-    maxPassengerCount: () => number,
+    maxPassengerCount: () => number;
     /** Gets the load factor of the elevator. 0 means empty, 1 means full. Varies with passenger weights, which vary -
      * not an exact measure. */
-    loadFactor: () => number,
+    loadFactor: () => number;
     /** Gets the direction the elevator is currently going to move toward. Can be "up", "down" or "stopped". */
-    destinationDirection: () => "up" | "down" | "stopped",
+    destinationDirection: () => "up" | "down" | "stopped";
     /** The current destination queue, meaning the floor numbers the elevator is scheduled to go to. Can be modified and 
      * emptied if desired. Note that you need to call `checkDestinationQueue()` for the change to take effect
      * immediately. */
-    destinationQueue: number[],
+    destinationQueue: number[];
     /** Checks the destination queue for any new destinations to go to. Note that you only need to call this if you
      * modify the destination queue explicitly. */
-    checkDestinationQueue: () => void,
+    checkDestinationQueue: () => void;
     /** Gets the currently pressed floor numbers as an array. */
-    getPressedFloors: () => number[],
+    getPressedFloors: () => number[];
     on: {
         /** Set the handler to be triggered when the elevator has completed all its tasks and is not doing anything. */
         (event: "idle", handler: () => void): void;
@@ -44,7 +44,7 @@ interface elevator {
 
 interface floor {
     /** Gets the floor number of the floor object. */
-    floorNum: () => number
+    floorNum: () => number;
     on: {
         /** Set the handler to be triggered when someone has pressed the up button at a floor. Note that passengers will
          * press the button again if they fail to enter an elevator. */
@@ -55,9 +55,16 @@ interface floor {
     }
 }
 
-const code =
+interface solution {
+    /** called when the challenge starts */
+    init: (elevators: elevator[], floors: floor[]) => void;
+    /** called repeatedly during the challenge */
+    update: (dt: number, elevators: elevator[], floors: floor[]) => void;
+}
+
+const solution: solution =
 {
-    init: function (elevators: elevator[], floors: floor[]) {
+    init: function (elevators, floors) {
         var elevator = elevators[0]; // Let's use the first elevator
 
         // Whenever the elevator is idle (has no more queued destinations) ...
@@ -67,7 +74,7 @@ const code =
             elevator.goToFloor(1);
         });
     },
-    update: function (dt: number, elevators: elevator[], floors: floor[]) {
+    update: function (dt, elevators, floors) {
         // We normally don't need to do anything here
     }
 };
