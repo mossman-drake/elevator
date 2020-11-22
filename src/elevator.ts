fc7cd1d5c64d8543fcc9d95c5475377fb3cbc36a
Drake Mossman <drake.mossman@covenanteyes.com>
2020-11-22 16:22:50 -0500

    Add exemplar for globals and extensions of objects

--- src/elevator.ts
+++ src/elevator.ts
@@ -1,18 +1,44 @@
-import { solution } from 'interface/api';
+import { elevator, floor } from 'interface/api';
 
-const solution: solution =
+interface elevatorExtensions {
+    /** Convenience function for getting the number of an elevator */
+    num: number,
+}
+interface floorExtensions {
+    /** Convenience function for getting the floor number. ! Not to be modified ! */
+    num: number,
+}
+interface solutionWithExtensions {
+    /** called when the challenge starts */
+    init: (elevators: elevator[], floors: floor[]) => void;
+    /** called repeatedly during the challenge */
+    update: (dt: number, elevators: (elevator & elevatorExtensions)[], floors: (floor & floorExtensions)[]) => void;
+}
+
+// Globals
+let time: number;
+
+const solution: solutionWithExtensions =
 {
-    init: function (elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+    init: function (baseElevators, baseFloors) {
+        // Init Globals
+        time = 0;
+        // Add customizations to objects
+        const elevators = baseElevators.map((elevator, index) => Object.assign(elevator, {
+            num: index,
+        } as elevatorExtensions));
+        const floors = baseFloors.map((floor, index) => Object.assign(floor, {
+            num: index,
+        } as floorExtensions));
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function () {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
+        // Logic
+        elevators.forEach((elevator) => {
+            elevator.on("idle", () => {
+                floors.forEach((floor) => elevator.goToFloor(floor.floorNum()));
+            })
         });
     },
     update: function (dt, elevators, floors) {
-        // We normally don't need to do anything here
+        time += dt;
     }
 };
